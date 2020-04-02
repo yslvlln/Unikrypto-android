5642d2b8869bb38464817a3edf39b599dc8155ab
yco <ycosilvallana@gmail.com>
2020-04-02 15:11:52 +0800

    Added nonce parameter

--- unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
+++ unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
@@ -9,30 +9,38 @@ import com.goterl.lazycode.lazysodium.utils.Key
 import com.goterl.lazycode.lazysodium.utils.KeyPair
 
 class Unikrypto {
-    private var ls: LazySodiumAndroid? = null
-    fun initSodium() : LazySodiumAndroid {
-        return ls ?: LazySodiumAndroid(SodiumAndroid())
-    }
-    fun krypted(publicKey: Key, secretKey: Key,  message: String): String? {
-        val keyPair = KeyPair(publicKey, secretKey)
-        val nonce = ls?.randomBytesBuf(SecretBox.NONCEBYTES)
-        try {
-            val ciphertext = ls?.cryptoBoxEasy(message, nonce, keyPair)
-            return ciphertext
-        } catch (e: SodiumException) {
-            e.printStackTrace()
-            return null
+    companion object {
+        private var ls: LazySodiumAndroid? = null
+        fun initSodium() : LazySodiumAndroid {
+            return ls ?: LazySodiumAndroid(SodiumAndroid())
         }
-    }
-    fun deckypted(publicKey: Key, secretKey: Key, ciphertext: String): String? {
-        val keyPair = KeyPair(publicKey, secretKey)
-        val nonce = ls?.randomBytesBuf(SecretBox.NONCEBYTES)
-        try {
-            val plaintext = ls?.cryptoBoxOpenEasy(ciphertext, nonce, keyPair)
-            return plaintext
-        } catch (e: SodiumException) {
-            e.printStackTrace()
-            return null
+        fun krypted(publicKey: Key, secretKey: Key,  message: String, nonce: ByteArray): String? {
+            val keyPair = KeyPair(publicKey, secretKey)
+            try {
+                val ciphertext = ls?.cryptoBoxEasy(message, nonce, keyPair)
+                return ciphertext
+            } catch (e: SodiumException) {
+                e.printStackTrace()
+                return null
+            }
+        }
+        fun deckypted(publicKey: Key, secretKey: Key, ciphertext: String, nonce: ByteArray): String? {
+            val keyPair = KeyPair(publicKey, secretKey)
+            try {
+                val plaintext = ls?.cryptoBoxOpenEasy(ciphertext, nonce, keyPair)
+                return plaintext
+            } catch (e: SodiumException) {
+                e.printStackTrace()
+                return null
+            }
+        }
+        fun generateNonce(): ByteArray? {
+            try {
+                return ls?.randomBytesBuf(SecretBox.NONCEBYTES)
+            } catch (e: SodiumException) {
+                e.printStackTrace()
+                return null
+            }
         }
     }
 }
