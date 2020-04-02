f6a75e2b34fec2e24caba03feb141499ce43448c
yco <ycosilvallana@gmail.com>
2020-04-02 12:29:24 +0800

    Initial commit

---
+++ unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
@@ -0,0 +1,36 @@
+package com.yslvlln.unikrypto
+
+import com.goterl.lazycode.lazysodium.LazySodiumAndroid
+import com.goterl.lazycode.lazysodium.SodiumAndroid
+import com.goterl.lazycode.lazysodium.exceptions.SodiumException
+import com.goterl.lazycode.lazysodium.interfaces.Box
+import com.goterl.lazycode.lazysodium.interfaces.SecretBox
+import com.goterl.lazycode.lazysodium.utils.Key
+import com.goterl.lazycode.lazysodium.utils.KeyPair
+
+class Unikrypto {
+    private var ls: LazySodiumAndroid? = null
+    fun initSodium() : LazySodiumAndroid {
+        return ls ?: LazySodiumAndroid(SodiumAndroid())
+    }
+    fun krypted(secretKey: Key, message: String): String? {
+        val nonce = ls?.randomBytesBuf(SecretBox.NONCEBYTES)
+        try {
+            val ciphertext = ls?.cryptoSecretBoxEasy(message, nonce, secretKey)
+            return ciphertext
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+    fun deckypted(secretKey: Key, ciphertext: String): String? {
+        val nonce = ls?.randomBytesBuf(SecretBox.NONCEBYTES)
+        try {
+            val plaintext = ls?.cryptoSecretBoxOpenEasy(ciphertext, nonce, secretKey)
+            return plaintext
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+}
