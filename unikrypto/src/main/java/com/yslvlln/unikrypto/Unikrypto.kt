df8cab13ad35248b772bf4ecd04158b807d1220e
yco <ycosilvallana@gmail.com>
2020-04-03 10:14:28 +0800

    Created a singleton

--- unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
+++ unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
@@ -6,28 +6,25 @@ import com.goterl.lazycode.lazysodium.exceptions.SodiumException
 import com.goterl.lazycode.lazysodium.utils.Key
 import java.lang.Exception
 
-class Unikrypto {
-    companion object {
-        fun krypted(secretkey: Key, message: String, nonce: ByteArray): String? {
-            val ls = LazySodiumAndroid(SodiumAndroid())
-            try {
-                val ciphertext = ls.cryptoSecretBoxEasy(message, nonce, secretkey)
-                return ciphertext
-            } catch (e: SodiumException) {
-                e.printStackTrace()
-                return null
-            }
+object Unikrypto {
+    val instance = LazySodiumAndroid(SodiumAndroid())
+    fun krypted(secretkey: Key, message: String, nonce: ByteArray): String? {
+        try {
+            val ciphertext = instance.cryptoSecretBoxEasy(message, nonce, secretkey)
+            return ciphertext
+        } catch (e: SodiumException) {
+            e.printStackTrace()
+            return null
         }
+    }
 
-        fun deckypted(secretKey: Key, ciphertext: String, nonce: ByteArray): String? {
-            val ls = LazySodiumAndroid(SodiumAndroid())
-            try {
-                val plaintext = ls.cryptoSecretBoxOpenEasy(ciphertext, nonce, secretKey)
-                return plaintext
-            } catch (e: SodiumException) {
-                e.printStackTrace()
-                return null
-            }
+    fun deckypted(secretKey: Key, ciphertext: String, nonce: ByteArray): String? {
+        try {
+            val plaintext = instance.cryptoSecretBoxOpenEasy(ciphertext, nonce, secretKey)
+            return plaintext
+        } catch (e: SodiumException) {
+            e.printStackTrace()
+            return null
         }
     }
 }
