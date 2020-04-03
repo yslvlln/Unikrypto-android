4656f1ef85f04840b6c898ab9820482440e4d964
yco <ycosilvallana@gmail.com>
2020-04-03 10:00:53 +0800

    Created ls variable

--- unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
+++ unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
@@ -8,34 +8,26 @@ import java.lang.Exception
 
 class Unikrypto {
     companion object {
-        lateinit var ls: LazySodiumAndroid
-        fun init() {
-            ls = LazySodiumAndroid(SodiumAndroid())
+        fun krypted(secretkey: Key, message: String, nonce: ByteArray): String? {
+            val ls = LazySodiumAndroid(SodiumAndroid())
+            try {
+                val ciphertext = ls.cryptoSecretBoxEasy(message, nonce, secretkey)
+                return ciphertext
+            } catch (e: SodiumException) {
+                e.printStackTrace()
+                return null
+            }
         }
-        fun krypted(secretkey: Key,  message: String, nonce: ByteArray): String? {
-            if (ls != null) {
-                try {
-                    val ciphertext = ls.cryptoSecretBoxEasy(message, nonce, secretkey)
-                    return ciphertext
-                } catch (e: SodiumException) {
-                    e.printStackTrace()
-                    return null
-                }
-            } else
-                throw Exception("Error initialization")
 
-        }
         fun deckypted(secretKey: Key, ciphertext: String, nonce: ByteArray): String? {
-            if (ls != null) {
-                try {
-                    val plaintext = ls.cryptoSecretBoxOpenEasy(ciphertext, nonce, secretKey)
-                    return plaintext
-                } catch (e: SodiumException) {
-                    e.printStackTrace()
-                    return null
-                }
-            } else
-                throw Exception("Error initialization")
+            val ls = LazySodiumAndroid(SodiumAndroid())
+            try {
+                val plaintext = ls.cryptoSecretBoxOpenEasy(ciphertext, nonce, secretKey)
+                return plaintext
+            } catch (e: SodiumException) {
+                e.printStackTrace()
+                return null
+            }
         }
     }
 }
