84c15de2c7bc3ffc67b4e072c80e5e4866824e7a
yco <ycosilvallana@gmail.com>
2020-04-03 14:09:28 +0800

    First release

--- unikrypto/src/main/java/com/yslvlln/unikrypto/SecretKey.kt
+++ unikrypto/src/main/java/com/yslvlln/unikrypto/SecretKey.kt
@@ -1,11 +1,20 @@
 package com.yslvlln.unikrypto
 
 import com.goterl.lazycode.lazysodium.utils.Key
+import java.lang.Exception
 
 object SecretKey {
-    var mKey: Key? = null
+    private var mKey: Key? = null
 
     fun setKey(key: String) {
         mKey = Key.fromPlainString(key)
+    }
+
+    fun getKey(): Key {
+        if (mKey != null) {
+            return mKey!!
+        } else {
+            throw Exception("Secret key is null")
+        }
     }
 }
--- unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
+++ unikrypto/src/main/java/com/yslvlln/unikrypto/Unikrypto.kt
@@ -2,13 +2,10 @@ package com.yslvlln.unikrypto
 
 import com.goterl.lazycode.lazysodium.LazySodiumAndroid
 import com.goterl.lazycode.lazysodium.SodiumAndroid
-import com.goterl.lazycode.lazysodium.exceptions.SodiumException
-import com.goterl.lazycode.lazysodium.utils.Key
-import java.lang.Exception
 
 object Unikrypto {
-    val instance = LazySodiumAndroid(SodiumAndroid())
-    val key = SecretKey.mKey
+    private val instance = LazySodiumAndroid(SodiumAndroid())
+    private val key = SecretKey.getKey()
 
     fun krypt(message: String, nonce: ByteArray): String? {
         return instance.cryptoSecretBoxEasy(message, nonce, key)
